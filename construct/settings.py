304c3f9f78a6b40d97ff734b22704142a18d46b6
Jhonathan Turial <31006977+jhonathanturial@users.noreply.github.com>
2024-01-09 13:17:46 -0300

    *

--- construct/settings.py
+++ construct/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuração Auth
+
+AUTH_USER_MODEL = 'usuarios.Users'
--- usuarios/models.py
+++ usuarios/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import AbstractUser
 
 class Users(AbstractUser):
     choices_cargo = (('V','Vendedor'),
-                     ('G', 'Gerente'),
+                     ('G', 'Gerente')
                      
                      )
     cargo = models.CharField(max_lenght=1, choices=choices_cargo)
